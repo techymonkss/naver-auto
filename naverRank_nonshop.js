b5b0d70a1a39f10f53e3318c3505391177399207
monk <techymonks@tuta.io>
2023-12-29 21:20:29 +0530

    Update naverRank_nonshop.js

--- naverRank_nonshop.js
+++ naverRank_nonshop.js
@@ -84,7 +84,7 @@ const nextPage = async (page) => {
 
 let msg = '';
 const getRank = async (searchText, siteUrl) => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
